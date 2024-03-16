cc8c2a4e21c45a101c1184a6b1fd5604cd36811b
Shahriar Faysal <160359045+shahriarfaysal@users.noreply.github.com>
2024-03-16 22:34:06 +0600

    Create comparison.java

---
+++ comparison.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Conditions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a == b)
+        {
+            System.out.println("Equal");
+        }
+        else
+        {
+           if(a > b) 
+           {
+               System.out.println("A is greater");
+           }
+           else
+           {
+               System.out.println("A is lesser");
+           }
+        }
+        
+    }
+}
